27bdc4b85cd2efd126d09a906905afe0afb97a9c
Matthew Erich <matthewerich1992@gmail.com>
2016-08-21 20:34:24 -0700

    Largest Array Algorithm Added
    added an algorithm to take multiple arrays, take a group of arrays and
    then pull the largest number from each and push them into a largest
    array.

--- FCC_Basic/BonfireFCC.js
+++ FCC_Basic/BonfireFCC.js
@@ -70,5 +70,23 @@ function titleCase(str){
 
 titleCase("I'm a little tea pot");
 
+// 6. Return the largest number in group of arrays?
+
+function largestOfFour (arr){
+	var results = [];
+	for(var i =0;i<arr.length;i++){
+		var largestNum=0;
+		
+		for(var j=0;j<arr[i].length;j++){
+			if(arr[i][j]>largestNum){
+				largestNum= arr[i][j];
+			}
+		}
+		results[i]= largestNum;
+	}
+	return results;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
