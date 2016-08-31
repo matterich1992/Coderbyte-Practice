f80a589a6e4de77fba7a95b4b686ddbf0685f819
Matthew Erich <matthewerich1992@gmail.com>
2016-08-30 22:43:45 -0700

    Added Algorithms to FCC Basic
    Added some array sort functions

--- randomAlgorithms/randomPlexars.js
+++ randomAlgorithms/randomPlexars.js
@@ -24,3 +24,33 @@ function calculate (str){
 }
 
 console.log(calculate("Bob has 5 apples and gains 6"));
+
+// Add the two smallest values in array (or largest)
+
+function twoSmallestSum (array){
+	array.sort(function(a,b){return a-b;})// for largest replace with b-a/ look up compare function for reasoing behind this
+	
+return array[0]+array[1];
+}
+
+twoSmallestSum([34,56,1,23,42]); // should return 24
+
+
+// find how many divisors there are in a number
+
+function getDivisorsCnt(num){
+    var results = [];
+    for(var i=1;i<=num;i++){
+      if(num%i===0){
+      results.push(i);
+      }
+    }
+    return results.length;
+}
+
+getDivisorsCnt(48); --> //10
+
+
+
+
+
